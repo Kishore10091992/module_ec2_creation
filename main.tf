80e22c7a311a45d1c3e51d96f9471cbc55352483
Kishore Kumar <kishorek1009@yahoo.com>
2025-07-15 15:30:00 +0530

    Update -2 main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ module "vpc" {
 
 module "security_group" {
  source = "./modules/security_group"
- vpc_id = module.main_vpc.vpc_id
+ vpc_id = module.vpc.vpc_id
  ingress {
   from_port = 0
   to_port = 0
