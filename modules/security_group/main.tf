8420ee436dd40daef47c94b78d31ad124b5aba56
Kishore Kumar <kishorek1009@yahoo.com>
2025-07-16 11:47:34 +0530

    Update main.tf

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -5,14 +5,14 @@ resource "aws_security_group" "main_sg" {
   from_port = var.from_port
   to_port = var.to_port
   protocol = var.protocol
-  cidr_block = var.default_ip
+  cidr_blocks = var.default_ip
  }
 
  egress {
   from_port = var.from_port
   to_port = var.to_port
   protocol = var.protocol
-  cidr_block = var.default_ip
+  cidr_blocks = var.default_ip
   }
 
   tags = var.tags
